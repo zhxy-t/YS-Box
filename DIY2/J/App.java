5ca8258a409c3af28e4849bd2288b78e2dc4808e
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-01-30 00:47:01 +0800

    Add files via upload

---
+++ DIY2/J/App.java
@@ -0,0 +1,93 @@
+package com.github.tvbox.osc.base;
+
+import android.app.Activity;
+import androidx.multidex.MultiDexApplication;
+
+import com.github.tvbox.osc.bean.VodInfo;
+import com.github.tvbox.osc.callback.EmptyCallback;
+import com.github.tvbox.osc.callback.LoadingCallback;
+import com.github.tvbox.osc.data.AppDataManager;
+import com.github.tvbox.osc.server.ControlManager;
+import com.github.tvbox.osc.util.AppManager;
+import com.github.tvbox.osc.util.EpgUtil;
+import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.OkGoHelper;
+import com.github.tvbox.osc.util.PlayerHelper;
+import com.github.tvbox.osc.util.js.JSEngine;
+import com.kingja.loadsir.core.LoadSir;
+import com.orhanobut.hawk.Hawk;
+
+import me.jessyan.autosize.AutoSizeConfig;
+import me.jessyan.autosize.unit.Subunits;
+
+/**
+ * @author pj567
+ * @date :2020/12/17
+ * @description:
+ */
+public class App extends MultiDexApplication {
+    private static App instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        initParams();
+        // OKGo
+        OkGoHelper.init(); //台标获取
+        EpgUtil.init();
+        // 初始化Web服务器
+        ControlManager.init(this);
+        //初始化数据库
+        AppDataManager.init();
+        LoadSir.beginBuilder()
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .commit();
+        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
+                .setSupportDP(false)
+                .setSupportSP(false)
+                .setSupportSubunits(Subunits.MM);
+        PlayerHelper.init();
+        JSEngine.getInstance().create();
+    }
+
+    private void initParams() {
+        // Hawk
+        Hawk.init(this).build();
+        Hawk.put(HawkConfig.DEBUG_OPEN, false);
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {   
+   Hawk.put(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+        Hawk.put(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+        Hawk.put(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+//         Hawk.put(HawkConfig.HOME_NUM, 2);       // History Number
+          Hawk.put(HawkConfig.DOH_URL, 2);        // DNS
+          Hawk.put(HawkConfig.PLAY_RENDER, 1);        // 渲染
+        //  Hawk.put(HawkConfig.PLAY_SCALE, 1);       //播放比例
+          //Hawk.put(HawkConfig.PLAY_TIME_STEP, 1);      
+//          Hawk.put(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture  
+        }
+    }
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        JSEngine.getInstance().destroy();
+    }
+
+
+    private VodInfo vodInfo;
+    public void setVodInfo(VodInfo vodinfo){
+        this.vodInfo = vodinfo;
+    }
+    public VodInfo getVodInfo(){
+        return this.vodInfo;
+    }
+
+    public Activity getCurrentActivity() {
+        return AppManager.getInstance().currentActivity();
+    }
+}
