f34b13ed9a18ff76a519c3c0a0b354acc4a26f2a
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-03 21:23:58 +0800

    Update DIY-Q2.sh

--- DIY-Q2.sh
+++ DIY-Q2.sh
@@ -65,7 +65,7 @@ cp $CURRENT_DIR/DIY2/J/首页/AppsAdapter.java         $CURRENT_DIR/$DIR/app/src
 cp $CURRENT_DIR/DIY2/J/首页/item_apps.xml            $CURRENT_DIR/$DIR/app/src/main/res/layout/item_apps.xml
 cp $CURRENT_DIR/DIY2/J/首页/activity_apps.xml         $CURRENT_DIR/$DIR/app/src/main/res/layout/activity_apps.xml
 cp $CURRENT_DIR/DIY2/J/首页/shape_user_delete.xml            $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_user_delete.xml
-
+cp $CURRENT_DIR/DIY2/J/首页/AppInfo.java            $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/bean/AppInfo.java
 //搜索修改
 cp $CURRENT_DIR/DIY2/J/搜索/dialog_checkbox_search.xml            $CURRENT_DIR/$DIR/app/src/main/res/layout/dialog_checkbox_search.xml
 cp $CURRENT_DIR/DIY2/J/搜索/SearchCheckboxDialog.java             $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/dialog/SearchCheckboxDialog.java
