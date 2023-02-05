c008beb8e99ab83657a57f902076477cec6b3602
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-06 01:20:59 +0800

    Update App.java

--- DIY2/J/App.java
+++ DIY2/J/App.java
@@ -1,14 +1,17 @@
 package com.github.tvbox.osc.base;
 
+import android.app.Activity;
 import androidx.multidex.MultiDexApplication;
 
+import com.github.tvbox.osc.bean.VodInfo;
 import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
 import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
+import com.github.tvbox.osc.util.AppManager;
 import com.github.tvbox.osc.util.EpgUtil;
+import com.github.tvbox.osc.util.FileUtils;
 import com.github.tvbox.osc.util.HawkConfig;
-//import com.github.tvbox.osc.util.LocaleHelper;
 import com.github.tvbox.osc.util.OkGoHelper;
 import com.github.tvbox.osc.util.PlayerHelper;
 import com.github.tvbox.osc.util.js.JSEngine;
@@ -31,11 +34,8 @@ public class App extends MultiDexApplication {
         super.onCreate();
         instance = this;
         initParams();
-        // takagen99 : Initialize Locale
-        initLocale();
         // OKGo
-        OkGoHelper.init();
-        // Get EPG Info
+        OkGoHelper.init(); //台标获取
         EpgUtil.init();
         // 初始化Web服务器
         ControlManager.init(this);
@@ -50,35 +50,21 @@ public class App extends MultiDexApplication {
                 .setSupportSP(false)
                 .setSupportSubunits(Subunits.MM);
         PlayerHelper.init();
-
-        // Add JS support
         JSEngine.getInstance().create();
+        FileUtils.cleanPlayerCache();
     }
 
     private void initParams() {
         // Hawk
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
-
-        putDefault(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
-        putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
-        putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
-//        putDefault(HawkConfig.HOME_NUM, 2);       // History Number
-//        putDefault(HawkConfig.DOH_URL, 2);        // DNS
-//        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture
-
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
+        }
     }
-
-
 
     public static App getInstance() {
         return instance;
-    }
-
-    private void putDefault(String key, Object value) {
-        if (!Hawk.contains(key)) {
-            Hawk.put(key, value);
-        }
     }
 
     @Override
@@ -87,4 +73,18 @@ public class App extends MultiDexApplication {
         JSEngine.getInstance().destroy();
     }
 
+
+    private VodInfo vodInfo;
+    public void setVodInfo(VodInfo vodinfo){
+        this.vodInfo = vodinfo;
+    }
+    public VodInfo getVodInfo(){
+        return this.vodInfo;
+    }
+
+    public Activity getCurrentActivity() {
+        return AppManager.getInstance().currentActivity();
+    }
 }
+
+
