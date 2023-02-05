9dad8ef22c8849cde97aa08998c9d146f25df1c2
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-05 20:14:49 +0800

    Update HomeIconDialog.java

--- DIY2/J/设置/HomeIconDialog.java
+++ DIY2/J/设置/HomeIconDialog.java
@@ -24,23 +24,30 @@ public class HomeIconDialog extends BaseDialog {
     private final TextView tvHomeMenu;
     private final TextView tvHomePush;
     private final TextView tvHomeDrive;
-    
+    private final TextView tvHomeHistory;
+    private final TextView tvHomeFavorite;
 
     public HomeIconDialog(@NonNull @NotNull Context context) {
         super(context);
         setContentView(R.layout.dialog_homeoption);
         setCanceledOnTouchOutside(true);
+        //搜索 设置
         tvHomeSearch = findViewById(R.id.tvHomeSearch);
         tvHomeSearch.setText(Hawk.get(HawkConfig.HOME_SEARCH_POSITION, true) ? "上方" : "下方");
         tvHomeMenu = findViewById(R.id.tvHomeMenu);
         tvHomeMenu.setText(Hawk.get(HawkConfig.HOME_MENU_POSITION, true) ? "上方" : "下方");
-        
+         //推送 应用     
         tvHomePush = findViewById(R.id.tvHomePush);
         tvHomePush.setText(Hawk.get(HawkConfig.HOME_PUSH_POSITION, true) ? "上方" : "下方");
         tvHomeDrive = findViewById(R.id.tvHomeDrive);
         tvHomeDrive.setText(Hawk.get(HawkConfig.HOME_APP_POSITION, true) ? "上方" : "下方");
+        //历史 收藏
+        tvHomeHistory = findViewById(R.id.tvHomeHistory);
+        tvHomeHistory.setText(Hawk.get(HawkConfig.HOME_HIST_POSITION, true) ? "上方" : "下方");
+        tvHomeFavorite = findViewById(R.id.tvHomeFavorite);
+        tvHomeFavorite.setText(Hawk.get(HawkConfig.HOME_FAV_POSITION, true) ? "上方" : "下方");
         
-        
+        //搜索
         findViewById(R.id.llSearch).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,7 +66,7 @@ public class HomeIconDialog extends BaseDialog {
         });
         
         
-        
+        //推送
            findViewById(R.id.llPush).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,7 +75,7 @@ public class HomeIconDialog extends BaseDialog {
                 tvHomePush.setText(Hawk.get(HawkConfig.HOME_PUSH_POSITION, true) ? "上方" : "下方");
             }
         });
-        
+        //应用
             findViewById(R.id.llDrive).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,7 +84,24 @@ public class HomeIconDialog extends BaseDialog {
                 tvHomeDrive.setText(Hawk.get(HawkConfig.HOME_APP_POSITION, true) ? "上方" : "下方");
             }
         });
-        
+        //收藏
+          findViewById(R.id.llFav).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FastClickCheckUtil.check(v);
+                Hawk.put(HawkConfig.HOME_FAV_POSITION, !Hawk.get(HawkConfig.HOME_FAV_POSITION, true));
+                tvHomeFavorite.setText(Hawk.get(HawkConfig.HOME_FAV_POSITION, true) ? "上方" : "下方");
+            }
+        });
+        //历史
+            findViewById(R.id.llHist).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FastClickCheckUtil.check(v);
+                Hawk.put(HawkConfig.HOME_HIST_POSITION, !Hawk.get(HawkConfig.HOME_HIST_POSITION, true));
+                tvHomeHistory.setText(Hawk.get(HawkConfig.HOME_HIST_POSITION, true) ? "上方" : "下方");
+            }
+        });
     }
 
 }
