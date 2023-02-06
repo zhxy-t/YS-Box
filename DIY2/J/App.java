485064f2f85e77e70a970d25c416b587670b7a24
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-06 13:14:37 +0800

    Update App.java

--- DIY2/J/App.java
+++ DIY2/J/App.java
@@ -64,14 +64,14 @@ public class App extends MultiDexApplication {
             Hawk.put(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
             Hawk.put(HawkConfig.HISTORY_NUM, 2);       // History Number
             Hawk.put(HawkConfig.DOH_URL, 2);        // DNS
-            Hawk.put(HawkConfig.PLAY_RENDER, 2);        // 渲染
+            Hawk.put(HawkConfig.PLAY_RENDER, 1);        // 渲染
             Hawk.put(HawkConfig.IJK_CACHE_PLAY, true);        //true=开, false=关  ijk缓存            
           Hawk.put(HawkConfig.HOME_SEARCH_POSITION, false);     // 搜索
-          Hawk.put(HawkConfig.HOME_MENU_POSITION, true);        // 设置
+          Hawk.put(HawkConfig.HOME_MENU_POSITION, false);        // 设置
           Hawk.put(HawkConfig.HOME_HIST_POSITION, false);     // 历史
-          Hawk.put(HawkConfig.HOME_PUSH_POSITION, true);        // 推送          
+          Hawk.put(HawkConfig.HOME_PUSH_POSITION, false);        // 推送          
           Hawk.put(HawkConfig.HOME_APP_POSITION, false);     // 应用
-          Hawk.put(HawkConfig.HOME_FAV_POSITION, true);        // 收藏    
+          Hawk.put(HawkConfig.HOME_FAV_POSITION, false);        // 收藏    
         // Hawk.put(HawkConfig.PLAY_SCALE, 1);       //播放比例
           //Hawk.put(HawkConfig.PLAY_TIME_STEP, 1);      
         // Hawk.put(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture   
