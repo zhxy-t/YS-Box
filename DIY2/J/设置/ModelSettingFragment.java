54f0a25208e4ec61e35ec4839b92625bc75736dd
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-17 18:33:41 +0800

    Update ModelSettingFragment.java

--- DIY2/J/设置/ModelSettingFragment.java
+++ DIY2/J/设置/ModelSettingFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.EditText;
 
 import android.widget.LinearLayout;
 
