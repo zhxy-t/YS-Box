950ff3317d7535fdda481a50d0964e37eeaa54be
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-19 20:44:25 +0800

    Update HomeActivity.java

--- DIY2/J/首页/HomeActivity.java
+++ DIY2/J/首页/HomeActivity.java
@@ -816,7 +816,7 @@ public class HomeActivity extends BaseActivity {
         ControlManager.get().stopServer();
     }
 
-    void showSiteSwitch() {
+   private void showSiteSwitch() {
         List<SourceBean> sites = ApiConfig.get().getSourceBeanList();
         if (sites.size() > 0) {
             SelectDialog<SourceBean> dialog = new SelectDialog<>(HomeActivity.this);
