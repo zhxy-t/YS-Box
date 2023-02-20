0daad893184c901f2d33e3b73328277ed2150b73
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-20 12:44:07 +0800

    Update HomeActivity.java

--- DIY2/J/首页/HomeActivity.java
+++ DIY2/J/首页/HomeActivity.java
@@ -142,6 +142,44 @@ public class HomeActivity extends BaseActivity {
         }
         initData();
     }
+	
+	
+	public class MarqueeView extends AppCompatTextView {
+
+    private static final String TAG = "MarqueeView";
+
+    public MarqueeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        if (focused) {
+            super.onFocusChanged(true, direction, previouslyFocusedRect);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        if (hasWindowFocus) {
+            super.onWindowFocusChanged(true);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    private void init() {
+        setSingleLine();
+        setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        setMarqueeRepeatLimit(-1);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+    }
+}
 
     private void initView() {
         this.tvWifi = findViewById(R.id.tvWifi);
