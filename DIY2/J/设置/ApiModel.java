1faf01f4487e25e708346689bb4e565b29e86e61
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-25 19:24:25 +0800

    Add files via upload

---
+++ DIY2/J/设置/ApiModel.java
@@ -0,0 +1,30 @@
+package com.github.tvbox.osc.bean;
+
+import java.io.Serializable;
+
+public class ApiModel implements Serializable {
+    private String name;
+    private String url;
+
+    public ApiModel() {
+        String str = "";
+        this.name = str;
+        this.url = str;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setName(String str) {
+        this.name = str;
+    }
+
+    public void setUrl(String str) {
+        this.url = str;
+    }
+}
