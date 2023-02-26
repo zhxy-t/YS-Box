fc5dceaaf550144fdfb99eb64da5832304cb1ecd
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-26 22:15:58 +0800

    Update PlayActivity.java

--- DIY2/J/点播/PlayActivity.java
+++ DIY2/J/点播/PlayActivity.java
@@ -1011,6 +1011,45 @@ public class PlayActivity extends BaseActivity {
             }
         }
         
+        JSONObject jsonParse(String str, String str2) throws JSONException {
+        JSONObject jSONObject = new JSONObject(str2);
+        String str2 = "data";
+        String str3 = "url";
+        String str2 = jSONObject.has(str2) ? jSONObject.getJSONObject(str2).getString(str3) : jSONObject.getString(str3);
+        String str4 = "";
+        jSONObject.optString("msg", str4);
+        if (str2.startsWith("//")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("https:");
+            stringBuilder.append(str2);
+            str2 = stringBuilder.toString();
+        }
+        if (!str2.startsWith("http")) {
+            return null;
+        }
+        JSONObject jSONObject2 = new JSONObject();
+        String optString = jSONObject.optString("user-agent", str4);
+        String str5 = " ";
+        if (optString.trim().length() > 0) {
+            StringBuilder stringBuilder2 = new StringBuilder();
+            stringBuilder2.append(str5);
+            stringBuilder2.append(optString);
+            jSONObject2.put("User-Agent", stringBuilder2.toString());
+        }
+        str = jSONObject.optString("referer", str4);
+        if (str.trim().length() > 0) {
+            StringBuilder stringBuilder3 = new StringBuilder();
+            stringBuilder3.append(str5);
+            stringBuilder3.append(str);
+            jSONObject2.put("Referer", stringBuilder3.toString());
+        }
+        jSONObject = new JSONObject();
+        jSONObject.put("header", jSONObject2);
+        jSONObject.put(str3, str2);
+        return jSONObject;
+    }
+        
+        
         if (Thunder.play(vs.url, new Thunder.ThunderCallback() {
             @Override
             public void status(int code, String info) {
@@ -1130,12 +1169,10 @@ public class PlayActivity extends BaseActivity {
     private void doParse(ParseBean pb) {
 
         stopParse();
-       // String str = "header";
-       // String str2 = "";
+
         JSONObject jSONObject;
         JSONObject optJSONObject;
-       // Iterator keys;
-        String str3;
+
         initParseLoadFound();
         if (pb.getType() == 0) {
             setTip("", true, false);
