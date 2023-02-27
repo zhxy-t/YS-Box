d9c31fe1801e8d6fe13c67e6c8eb2cdb9fc750a5
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-27 13:43:40 +0800

    Update SourceUtil.java

--- DIY2/J/设置/SourceUtil.java
+++ DIY2/J/设置/SourceUtil.java
@@ -33,15 +33,15 @@ public class SourceUtil {
             setCurrentApi((ApiModel) history.get(0));
         }
     }
-/*
+
     public static void loadStoreHouse(String str, Callback<Map<String, String>> callback) {
         Iterator it = ((JsonObject) new Gson().fromJson(str, JsonObject.class)).getAsJsonArray("storeHouse").iterator();
         while (it.hasNext()) {
             JsonObject jsonObject = (JsonObject) ((JsonElement) it.next());
-            //httpGet(jsonObject.get("sourceUrl").getAsString().trim(), new 4(jsonObject, callback));
+            httpGet(jsonObject.get("sourceUrl").getAsString().trim(), new 4(jsonObject, callback));
         }
     }
-*/
+
     public static List<ApiModel> putHistory(List<ApiModel> list) {
         historyMap = new HashMap();
         history = list;
@@ -54,11 +54,8 @@ public class SourceUtil {
 
     public static ApiModel setCurrentApi(String str) {
         String apiName = getApiName(str);
-
-        Hawk.put(HawkConfig.API_NAME, str);
-        Hawk.put(HawkConfig.API_URL, str);
-        
-
+        Hawk.put("api_name", apiName);
+        Hawk.put("api_url", str);
         ApiModel apiModel = new ApiModel();
         apiModel.setUrl(str);
         apiModel.setName(apiName);
@@ -74,11 +71,10 @@ public class SourceUtil {
         return history;
     }
 
-    /*
     public static void httpGet(String str, Callback<String> callback) {
         ((GetRequest) ((GetRequest) ((GetRequest) JsonObject.get(str).headers("User-Agent", userAgent)).headers("Accept", requestAccept)).cacheMode(CacheMode.NO_CACHE)).execute(new 1(callback));
     }
-*/
+
     public static Integer indexOf(String str) {
         for (int i = 0; i < history.size(); i++) {
             if (StringUtils.equals(((ApiModel) history.get(i)).getUrl(), str)) {
@@ -133,10 +129,8 @@ public class SourceUtil {
 
     public static void clearCurrentApi() {
         String str = "";
-        //Hawk.put("api_name", str);
-        //Hawk.put("api_url", str);
-        Hawk.put(HawkConfig.API_NAME, str);
-        Hawk.put(HawkConfig.API_URL, str);
+        Hawk.put("api_name", str);
+        Hawk.put("api_url", str);
     }
 
     public static String getApiName(String str) {
@@ -149,11 +143,11 @@ public class SourceUtil {
         Hawk.put("api_url", apiModel.getUrl());
         return apiModel;
     }
-/*
+
     public static void addSource(String str, Callback<String> callback) {
         httpGet(str, new 2(callback));
     }
-*/
+
     public static List<ApiModel> getHistory() {
         return history;
     }
@@ -165,9 +159,8 @@ public class SourceUtil {
     public static List<ApiModel> getHistoryFromDB(List<ApiModel> list) {
         return (List) Hawk.get("api_history", list);
     }
-/*
+
     public static void replaceAllSource(String str, Callback<String> callback) {
         httpGet(str, new 3(callback));
     }
-    */
 }
