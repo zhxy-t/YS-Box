5c58048ec4e56214d54ebc7628d67b955856894c
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-02-27 20:00:17 +0800

    Update HomeActivity.java

--- DIY2/J/首页/HomeActivity.java
+++ DIY2/J/首页/HomeActivity.java
@@ -842,7 +842,7 @@ public class HomeActivity extends BaseActivity {
             ViewGroup.LayoutParams clp = cl_root.getLayoutParams();
             //clp.width = AutoSizeUtils.mm2px(dialog.getContext(), 380+200*spanCount);;
             dialog.setTip("首页固定数据源");
-	         dialog.a(new a(this), new b(this), sites, sites.indexOf(ApiConfig.get().getHomeSourceBean()));
+	         dialog.a(TipDialog.OnListener(), new Runnable(this), sites, sites.indexOf(ApiConfig.get().getHomeSourceBean()));
 		
             dialog.show();
 		
