a13e691ea8c8470a3987b7537012d81b7e54befe
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-03-19 10:11:59 +0800

    Update SettingActivity.java

--- DIY2/J/设置/SettingActivity.java
+++ DIY2/J/设置/SettingActivity.java
@@ -181,16 +181,16 @@ public class SettingActivity extends BaseActivity {
         return super.dispatchKeyEvent(event);
     }
     
-    
+    //& (currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")))
  public void onBackPressed() {
         if ((homeSourceKey != null && !homeSourceKey.equals(Hawk.get(HawkConfig.HOME_API, ""))) ||
                 !currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) ||
                 !currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")) ||
-                !currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")) ||
+                //!currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")) ||
                 homeRec != Hawk.get(HawkConfig.HOME_REC, 0) ||
                 dnsOpt != Hawk.get(HawkConfig.DOH_URL, 0)) {
             AppManager.getInstance().finishAllActivity();
-            if (currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) & (currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")) & (currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, ""))))) {
+            if (currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) & (currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")))) {
                 Bundle bundle = new Bundle();
                 bundle.putBoolean("useCache", true);
                 jumpActivity(HomeActivity.class, bundle);
